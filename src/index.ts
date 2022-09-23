f25a50070465c239d3d2b206ef9556e8c2c10dee
Johann Schopplich <mail@johannschopplich.com>
2022-09-23 09:43:04 +0200

    chore!: drop acl default export for named export only

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export * from './acl'
-export { ACL as default } from './acl'
 export type { AclConfig, AclSet, AclSetPropertyKey } from './types'
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import type { MaybeArray, Nullable } from './types'
 
-export const toArray = <T>(array?: Nullable<MaybeArray<T>>): Array<T> => {
+export function toArray<T>(array?: Nullable<MaybeArray<T>>): Array<T> {
   array = array || []
   if (Array.isArray(array))
     return array
